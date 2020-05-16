55ad4e359e351c40a8c9453e2c5059dda158113d
Carelynn Tsai <carelynntsai@gmail.com>
2020-05-16 14:13:19 -0400

    more packages

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const Alexa = require('ask-sdk-core');
 const { ExpressAdapter } = require('ask-sdk-express-adapter');
 
 const app = express();
+const port = 3000;
 const skillBuilder = Alexa.SkillBuilders.custom();
 const skill = skillBuilder.create();
 const adapter = new ExpressAdapter(skill, true, true);
 
 app.post('/', adapter.getRequestHandlers());
-app.listen(3000);
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
